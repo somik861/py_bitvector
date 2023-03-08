35561c3fa50da2e5c68fe7ac1e573ad3d0142015
Jan Juracka <janjuracka861@seznam.cz>
2023-03-08 16:55:57 +0100

    ADD: basic ops, wrappers, examples

--- bitvector.py
+++ bitvector.py
@@ -1,5 +1,7 @@
 from math import log2
 from itertools import dropwhile
+from typing import Any, Callable
+from operator import and_, or_
 
 DEFAULT_SIZE = 64
 
@@ -40,6 +42,8 @@ class BitVector:
         bits = []
         for val in dropwhile(lambda x: not x, self.value):
             bits.append(str(int(val)))
+        if not bits:
+            bits.append('0')
 
         out = []
         for i, val in enumerate(reversed(bits)):
@@ -52,7 +56,75 @@ class BitVector:
         return ''.join(['0b'] + out)
 
     def __str__(self) -> str:
-        return f'{self.as_int()} ~ {self.as_bits()}'
+        return f'[{len(self)}] {self.as_int()}~{self.as_bits()}'
 
     def __repr__(self) -> str:
         return f'BitVector({self.as_int()}, size={len(self.value)})'
+
+    def __len__(self) -> int:
+        return len(self.value)
+
+    def __getitem__(self, idx: int) -> bool:
+        assert idx < len(
+            self), f'Index {idx} out of range; range: [0, {len(self)})'
+        return self.value[-idx - 1]
+
+    def __setitem__(self, idx: int, new_val: bool | int) -> None:
+        assert idx < len(
+            self), f'Index {idx} out of range; range: [0, {len(self)})'
+
+        new_val = bool(new_val)
+        self.value[-idx - 1] = new_val
+
+
+def _bv_test(f: Callable[[BitVector, BitVector, int], BitVector]) -> Any:
+    def wrapper(lhs: BitVector, rhs: BitVector, size: int | None = None, *args, **kwargs) -> BitVector:
+        if size is not None:
+            assert len(lhs) == len(rhs) and size <= len(
+                lhs), f'Missmatching sizes: {len(lhs)=} {len(rhs)=} has to be bigger than {size=}'
+        else:
+            assert len(lhs) == len(
+                rhs), f'Missmatching sizes: {len(lhs)=} {len(rhs)=}'
+            size = len(lhs)
+
+        return f(lhs, rhs, size, *args, **kwargs)
+
+    return wrapper
+
+
+class ops:
+    @staticmethod
+    @_bv_test
+    def _noop(lhs: BitVector, rhs: BitVector, size: int) -> BitVector:
+        pass
+
+    @staticmethod
+    @_bv_test
+    def elem_wise(lhs: BitVector, rhs: BitVector, size: int, f: Callable[[bool, bool], bool], ) -> BitVector:
+        out = BitVector(size=len(lhs))
+        for i in range(size):
+            out[i] = f(lhs[i], rhs[i])
+        return out
+
+    @staticmethod
+    @_bv_test
+    def eq(lhs: BitVector, rhs: BitVector, size: int) -> BitVector:
+        for i in range(size):
+            if lhs[i] != rhs[i]:
+                return False
+        return True
+
+    @staticmethod
+    @_bv_test
+    def neq(lhs: BitVector, rhs: BitVector, size: int) -> BitVector:
+        return not ops.eq(lhs, rhs, size)
+
+    @staticmethod
+    @_bv_test
+    def bit_and(lhs: BitVector, rhs: BitVector, size: int) -> BitVector:
+        return ops.elem_wise(lhs, rhs, size, and_)
+
+    @staticmethod
+    @_bv_test
+    def bit_or(lhs: BitVector, rhs: BitVector, size: int) -> BitVector:
+        return ops.elem_wise(lhs, rhs, size, or_)
--- example.py
+++ example.py
@@ -1,9 +1,29 @@
-from bitvector import BitVector
+from bitvector import BitVector, ops
 
 
-bv = BitVector(32)
+bv32 = BitVector(32)
+bv15 = BitVector(15)
+bv3 = BitVector(3)
 
-print(bv)
-print(bv.as_bits())
-print(bv.as_int())
-print(bv.as_hex())
+print(f'{bv32 = !s}')
+print(f'{bv15 = !s}')
+print(f'{bv3 = !s}')
+
+print()
+
+print(f'{bv32.as_bits() = !s}')
+print(f'{bv32.as_int() = !s}')
+print(f'{bv32.as_hex() = !s}')
+print(f'{ops.eq(bv32, bv32) = }')
+
+print()
+
+print(f'{ops.eq(bv32, bv15) = }')
+print(f'{ops.eq(bv15, bv3) = }')
+print(f'{ops.eq(bv15, bv3, size=2) = }')
+
+print()
+
+print(f'{ops.bit_and(bv15, bv3) = !s}')
+print(f'{ops.bit_or(bv32, bv15) = !s}')
+print(f'{ops.bit_or(bv32, bv15, size=5) = !s}')
